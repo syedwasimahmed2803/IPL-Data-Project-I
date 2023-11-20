0d6b025a1654a5d71be09cde58c904c6f3500ed9
Syed Wasim Ahmed <syedwasimahmed2803@gmail.com>
2023-11-20 16:20:26 +0530

    Made changes to the solution based on problem statement

--- src/server/2-matches-won-per-team-per-year.js
+++ src/server/2-matches-won-per-team-per-year.js
@@ -1,11 +1,29 @@
+// function matchesWonPerTeam(matches) {
+//   return matches.reduce((wins, match) => {
+//     const { season, winner } = match;
+//     if (winner != "") {
+//       wins[season] = wins[season] || {};
+//       wins[season][winner] = (wins[season][winner] || 0) + 1;
+//     }
+//     return wins;
+//   }, {});
+// }
+
 function matchesWonPerTeam(matches) {
-  return matches.reduce((wins, match) => {
-    const { season, winner } = match;
+  return matches.reduce((acc, curr) => {
+    const { winner, season } = curr;
     if (winner != "") {
-      wins[season] = wins[season] || {};
-      wins[season][winner] = (wins[season][winner] || 0) + 1;
+      if (!acc[winner]) {
+        acc[winner] = {};
+      }
+
+      if (!acc[winner][season]) {
+        acc[winner][season] = 1;
+      }
+      acc[winner][season] += 1;
     }
-    return wins;
+    return acc;
   }, {});
 }
+
 module.exports.matchesWonPerTeam = matchesWonPerTeam;
